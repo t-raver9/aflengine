d0206cb5ad3f4bd70462df8f99ff07fa1ef76ab4
Chris <chris.strods@gmail.com>
2018-02-26 22:30:43 +1100

    Added player stats scraping
    Former-commit-id: c3bbbd09c33bae64a181511d9a8cabdd12235d7c

--- process/getmatches.py
+++ process/getmatches.py
@@ -6,38 +6,79 @@ Created on Wed Feb  7 20:54:07 2018
 @author: chrisstrods
 """
 
-import urllib
 from bs4 import BeautifulSoup
-import json
+import urllib, os, json, sys
 
-base = 'https://afltables.com/afl/seas/'
-year = 2017
-end = ".html"
-matchlist = dict()
 
-while(year>1896):
-    url = base + str(year) + end
 
-    page = urllib.request.urlopen(url)
+#startyear - earliest year to get
+#endyear - lastest year to get
+def getPageNames(startyear, endyear):  #Gets JSON list of URLS
+    base = 'https://afltables.com/afl/seas/'
+    end = ".html"
+    matchlist = dict()
+    year = endyear
 
-    soup = BeautifulSoup(page, 'html.parser')
+    #loops backwards from endyear to startyear
+    while(year>=startyear):
+        url = base + str(year) + end
+        
+        #get page containing all match links in the season
+        page = urllib.request.urlopen(url)
+        soup = BeautifulSoup(page, 'html.parser')
 
-    matches = list()
+        matches = list()
 
-    for a in soup.findAll('a',href=True):
+        #Get all links on the page
+        for a in soup.findAll('a',href=True):
+            
+            #If link text is 'match stats' then it links to a game, so
+            #record that URL in the list
+            if(a.text=="Match stats"):
+                matchstring = "https://afltables.com/afl/" + str(a['href'])[3:]
+                matches.append(matchstring)
+                matchlist.update({year:matches})
+        print("Successfully downloaded match codes for " + str(year) + " season")
+        year -= 1
+        
+
+    #When all seasons are done, output to JSON list
+    with open('../matchfiles/matchlist.json','w') as fout:
+        json.dump(matchlist,fout)
+
+#Get files from JSON list and store them in the        
+def getPages():
+    #load list from JSON file
+    with open('../matchfiles/matchlist.json','r') as fin:
+        data = json.load(fin)
     
-        if(a.text=="Match stats"):
-            matchstring = "https://afltables.com/afl/" + str(a['href'])[3:]
-            matches.append(matchstring)
-            matchlist.update({year:matches})
-    year -= 1
+    #Iterate through each year in the list
+    for year, mlist in data.items():
+        print("Downloading " + str(year) + " season")
+        #Create folder for that year if it doesn't exist
+        if not os.path.exists("../matchfiles/" + year):
+            os.makedirs("../matchfiles/" + year)
+        #Iterate through each match in the year
+        for matchurl in mlist:
+            code = str(matchurl).rpartition('/')[2]
+            urllib.request.urlretrieve(matchurl, 
+                "../matchfiles/" + year + "/" + code)
+        print("Successfully downloaded matches for " + str(year) + " season") 
+            
 
-with open('matchlist.json','w') as fout:
-    json.dump(matchlist,fout)
     
+#If run from command line, takes year ranges as parameters, otherwise
+#uses defaults
+if __name__ == '__main__':
+    if(len(sys.argv) != 3):
+        print("Using default season range of 1897 to 2017")
+        syear = 1897
+        eyear = 2017
+    else:
+        syear = int(sys.argv[1])
+        eyear = int(sys.argv[2])
     
-    
-
-
+    getPageNames(syear,eyear)
+    getPages()
 
  
--- process/scrapefunctions.py
+++ process/scrapefunctions.py
@@ -6,20 +6,21 @@ Created on Tue Feb 20 20:08:44 2018
 @author: chrisstrods
 """
 from bs4 import BeautifulSoup
-import urllib
 import pandas as pd
 import re
+import organise as o
 
-
+#Load an html page and return it as s BS table
 def loadPage(u):
-    page = urllib.request.urlopen(u)
+    #page = urllib.request.urlopen(u)
 
-    soup = BeautifulSoup(page, 'html.parser')
+    soup = BeautifulSoup(open(u), 'html.parser')
 
     tables = soup.findChildren('table')
 
     return tables
 
+#Process and return match summary box
 def getSummary(t):
     cells=list()
     for cell in t.findAll("td"):
@@ -28,82 +29,97 @@ def getSummary(t):
         
     matchstring = str(cells[1]).split(" ")
     
-    if(len(cells)==25):
+    
+    if(len(cells)==25): #Game finishing in regular time
         umpstring = str(cells[24]).split(",")
-    elif(len(cells)==29):
+    elif(len(cells)==29): #Game finishing in overtime
         umpstring = str(cells[28]).split(",")
     else:
-        umpstring = "WRONG"
+        umpstring = "ERROR"
     
-    
+    #If match is a final, remove 'FINAL' cell so that it aligns with
+    #Round numbers
     if(str(matchstring[2]) == "Final"):
         del matchstring[2]
     
     
-        
-    outrow = [None] * 22
+    #Create blank row to be filled    
+    outrow = [None] * 25
     outrow[0] = matchstring[1] #round
 
 
-    if(len(matchstring) == 14): #if venue name is one word
+    if(len(matchstring) == 14): #Venue name two words
         outrow[1] = matchstring[3] + " " + matchstring[4] #venue
-        outrow[2] = matchstring[7] #date
+        outrow[2] = str(matchstring[7]) #date
+        outrow[24] = str(matchstring[7].split("-")[2])
         outrow[3] = matchstring[6].replace(",","") #day
         outrow[4] = matchstring[8] #localtime
         outrow[5] = matchstring[13] #venue
-    elif(len(matchstring) == 13):
+    elif(len(matchstring) == 13): #Venue name one word
         outrow[1] = matchstring[3] #venue
-        outrow[2] = matchstring[6] #date
+        outrow[2] = str(matchstring[6]) #date
+        outrow[24] = str(matchstring[6].split("-")[2])
         outrow[3] = matchstring[5].replace(",","")  #day
         outrow[4] = matchstring[7] #localtime
         outrow[5] = matchstring[12] #venue
-    elif(len(matchstring) == 12):
+    elif(len(matchstring) == 12): #Venue name two words, no crowd
         outrow[1] = matchstring[3] + " " + matchstring[4] #venue
-        outrow[2] = matchstring[7] #date
+        outrow[2] = str(matchstring[7]) #date
+        outrow[24] = str(matchstring[7].split("-")[2])
         outrow[3] = matchstring[6].replace(",","")  #day
         outrow[4] = matchstring[8] #localtime
         outrow[5] = matchstring[11] #venue
-    elif(len(matchstring) == 11):
+    elif(len(matchstring) == 11): #Venue name one word, no crowd
         outrow[1] = matchstring[3] #venue
-        outrow[2] = matchstring[6] #date
+        outrow[2] = str(matchstring[6]) #date
+        outrow[24] = str(matchstring[6].split("-")[2])
         outrow[3] = matchstring[5].replace(",","")  #day
         outrow[4] = matchstring[7] #localtime
         outrow[5] = matchstring[10] #venue
-    elif(len(matchstring) == 10):
+    elif(len(matchstring) == 10): #Venue name two words, no venue or timezeone
         outrow[1] = matchstring[3] + " " + matchstring[4] #venue
-        outrow[2] = matchstring[7] #date
+        outrow[2] = str(matchstring[7]) #date
+        outrow[24] = str(matchstring[7].split("-")[2])
         outrow[3] = matchstring[6].replace(",","")  #day
         outrow[4] = matchstring[8] #localtime
-        #outrow[5] = matchstring[10] #venue 
-    elif(len(matchstring) == 9):
+    elif(len(matchstring) == 9): #Venue name one word, no venue or timezeone
         outrow[1] = matchstring[3] #venue
-        outrow[2] = matchstring[6] #date
+        outrow[2] = str(matchstring[6]) #date
+        outrow[24] = str(matchstring[6].split("-")[2])
         outrow[3] = matchstring[5].replace(",","")  #day
         outrow[4] = matchstring[7] #localtime
-        #outrow[5] = matchstring[10] #venue
-    elif(len(matchstring) == 8):
-        outrow[1] = matchstring[3] #venue
-        outrow[2] = matchstring[6] #date
-        outrow[3] = matchstring[5].replace(",","")  #day
-        outrow[4] = matchstring[7] #localtime
-        #outrow[5] = matchstring[10] #venue
     else:
         print ("Error with file:" + str(cells[1]) + "   " + str(len(matchstring)))
         
     
-    #outrow[1]
-    outrow[6] = cells[3]    #hteam
-    outrow[7] = cells[4]    #hteamQ1
-    outrow[8] = cells[5]    #hteamQ2
-    outrow[9] = cells[6]    #hteamQ3
-    outrow[10] = cells[7]   #hteamQ4
-    outrow[11] = cells[8]   #ateam
-    outrow[12] = cells[9]   #ateamQ1
-    outrow[13] = cells[10]  #ateamQ2
-    outrow[14] = cells[11]  #ateamQ3
-    outrow[15] = cells[12]  #ateamQ4
+    #Process teams and quarter by quarter scores for non overtime games
+    if(len(cells)==25): #Game finishing in regular time
+        outrow[6] = cells[3]    #hteam
+        outrow[7] = cells[4]    #hteamQ1
+        outrow[8] = cells[5]    #hteamQ2
+        outrow[9] = cells[6]    #hteamQ3
+        outrow[10] = cells[7]   #hteamQ4
+        outrow[11] = cells[8]   #ateam
+        outrow[12] = cells[9]   #ateamQ1
+        outrow[13] = cells[10]  #ateamQ2
+        outrow[14] = cells[11]  #ateamQ3
+        outrow[15] = cells[12]  #ateamQ4
+    elif(len(cells)==29): #Game finishing in overtime
+        outrow[6] = cells[3]    #hteam
+        outrow[7] = cells[4]    #hteamQ1
+        outrow[8] = cells[5]    #hteamQ2
+        outrow[9] = cells[6]    #hteamQ3
+        outrow[10] = cells[7]   #hteamQ4
+        outrow[11] = cells[9]   #ateam
+        outrow[12] = cells[10]   #ateamQ1
+        outrow[13] = cells[11]  #ateamQ2
+        outrow[14] = cells[12]  #ateamQ3
+        outrow[15] = cells[13]  #ateamQ4
+        outrow[22] = cells[8]  #hteamET
+        outrow[23] = cells[14]  #ateamET
+       
     
-    
+    #Process differently based on howm any umpires in the game
     if(len(umpstring)>2):
         outrow[16] =   umpstring[0].split("(")[0]
         outrow[17] =   umpstring[1].split("(")[0]#umpire2    
@@ -128,14 +144,94 @@ def getSummary(t):
     return outrow
 
 
+
+
+def getPlayerStats(p,t,ha,s):
+    rows=list()
+    for row in t.findAll("tr"):
+        rows.append(row)
+    
+    if(len(rows) != 26 and len(rows) != 27):
+        print("FUCKED UP")
+    else:
+        rowpoint = 2
+        
+        #loop through each player in team
+        while(int(rowpoint)<=23):
+            outrow = [None] * 30
+            cells = list()
+            for cell in rows[rowpoint].findAll("td"):
+                cells.append(cell.text)
+                
+            playerID = rows[rowpoint].find("a",href=True)['href'].split("/")[4].split(".")[0]
+            team = rows[0].text.split(" Match")[0]
+                
+            
+            
+            
+            
+            outrow[0] = playerID
+            outrow[1] = o.getMatchIndex(s)
+            outrow[2] = o.replaceTeam(team)
+            outrow[3] = ha
+            outrow[4] = cells[1].split(",")[1]
+            outrow[5] = cells[1].split(",")[0]
+            outrow[6] = cells[0]
+            outrow[7] = cells[2]
+            outrow[8] = cells[3]
+            outrow[9] = cells[4]
+            outrow[10] = cells[5]
+            outrow[11] = cells[6]
+            outrow[12] = cells[7]
+            outrow[13] = cells[8]
+            outrow[14] = cells[9]
+            outrow[15] = cells[10]
+            outrow[16] = cells[11]
+            outrow[17] = cells[12]
+            outrow[18] = cells[13]
+            outrow[19] = cells[14]
+            outrow[20] = cells[15]
+            outrow[21] = cells[16]
+            outrow[22] = cells[17]
+            outrow[23] = cells[18]
+            outrow[24] = cells[19]
+            outrow[25] = cells[20]
+            outrow[26] = cells[21]
+            outrow[27] = cells[22]
+            outrow[28] = cells[23]
+            outrow[29] = cells[24]
+            p.loc[len(p)] = outrow
+            rowpoint += 1    
+        return p
+
+
+
+
+
+
+#Output row to the table
 def initSummaries():
     return pd.DataFrame(columns = ['round','venue','date','day',
                                    'time','crowd','hteam','hteam_q1',
                                    'hteam_q2','hteam_q3','hteam_q4',
-                                   'ateam','hteam_q1','hteam_q2',
-                                   'hteam_q3','hteam_q4','umpire1',
+                                   'ateam','ateam_q1','ateam_q2',
+                                   'ateam_q3','ateam_q4','umpire1',
                                    'umpire2','umpire3','umpire1games',
-                                   'umpire2games','umpire3games'])
+                                   'umpire2games','umpire3games',
+                                   'hteam_et','ateam_et','season'])
 
+def initPlayerStats():
+    return pd.DataFrame(columns=['playerid','matchid','team','ha','first_name','last_name',
+                                   'number','kicks','marks','handballs',
+                                   'disposals','goals','behinds',
+                                   'hitouts','tackles','rebound50',
+                                   'inside50','clearances','clangers',
+                                   'frees_for','frees_against',
+                                   'brownlow','contested_poss',
+                                   'uncontested_poss','contested_marks',
+                                   'marks_in_50','one_percenters',
+                                   'bounces','goal_assists','tog'])
 
+  
+    
     
