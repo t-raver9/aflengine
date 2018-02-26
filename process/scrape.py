c3bbbd09c33bae64a181511d9a8cabdd12235d7c
Chris <chris.strods@gmail.com>
2018-02-26 22:30:43 +1100

    Added player stats scraping

--- process/scrape.py
+++ process/scrape.py
@@ -7,36 +7,89 @@ Created on Wed Feb  7 20:31:18 2018
 """
 
 
-import functions as f
+import scrapefunctions as f
 import os
-
-folder ='/Users/chrisstrods/AnacondaProjects/aflengine/matchfiles/'
-year = 2017
-
-
-match_summaries = f.initSummaries()
-
-while(year>1896):
-    print("Processing year: " + str(year))
-    files = os.listdir(folder + str(year))
+import sys
+from os.path import dirname, abspath
 
 
-    for file in files:
-        rawmatch = f.loadPage("file://" + folder + str(year) + "/" + file)
-        match_summaries.loc[len(match_summaries)] = f.getSummary(rawmatch[0])
+def scrape(syear,eyear):
+
+
+    d = dirname(dirname(abspath(__file__)))
+    startyear = syear
+    endyear = eyear
+    year = endyear
+    match_summaries = f.initSummaries()
+    player_stats = f.initPlayerStats()
+    #scoring_progression = f.initScoringProgression()
     
-    year -= 1
+    #iterate through each year, run the scraping process
+    while(int(year)>=int(startyear)):
+        print("Processing year: " + str(year))
+        
+        
+        files = os.listdir(d + "/matchfiles/" + str(year))
+        
+        #Iterate through each match in the year
+        for file in files:
+            
+            #Load the match HTML
+            rawmatch = f.loadPage(d + "/matchfiles/" + str(year) + "/" + file)
+            
+            #removes the 'records' table if there is one
+            if(len(rawmatch) == 9):
+                del rawmatch[1]
+            elif(len(rawmatch) == 8 and int(year) <= 2007):
+                del rawmatch[1]
+            
+            #Scrape the Match Summary
+            match_summaries.loc[len(match_summaries)] = f.getSummary(rawmatch[0])
+            
+            #Scrape the player stats
+            player_stats = f.getPlayerStats(player_stats,rawmatch[2],'H',rawmatch[0])
+            player_stats = f.getPlayerStats(player_stats,rawmatch[4],'A',rawmatch[0])
+            
+            #Scrape the scoring progression (if applicable)
+            #TODO
+            
+        year -= 1
+            
+
+    #return match_summaries, player_stats
+    return player_stats
+    
+    
+
+         
+            
+#If run from command line, takes year ranges as parameters, otherwise
+#uses defaults
+if __name__ == '__main__':
+    if(len(sys.argv) != 3):
+        print("Using default season range of 1897 to 2017")
+        syear = 2017
+        eyear = 2017
+    else:
+        syear = int(sys.argv[1])
+        eyear = int(sys.argv[2])
+    
+    #summaries, players = scrape(syear,eyear)
+    players=scrape(syear,eyear)
+    
+    
+    #Output to CSV
+    #summaries.to_csv("../outputs/match_summaries.csv", mode="w")
+    #players.to_csv("../outputs/player_stats.csv", mode="w")   
 
 
 
-match_summaries.to_csv("./outputs/match_summaries2.csv", mode="w")
+
+
+
+
 
 #rawmatch = f.loadPage(folder)
-
-
-
-
-
 #match_summary= match[0]
 #home_stats= match[2]
 #away_stats= match[4]
