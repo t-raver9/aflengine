79767bc96d68c7484af6ccfdd9075372cb63dfd5
Harry Travers <33503958+t-raver9@users.noreply.github.com>
2020-02-13 22:04:10 +1100

    Code to aggregate player data to matches

--- analysis/machine_learning/main.py
+++ analysis/machine_learning/main.py
@@ -3,6 +3,7 @@ from ladders import generate_ladder_objects, add_ladders_to_dataset
 from os.path import dirname, abspath
 from team_form import last_x_games_stats_hteam, last_x_games_stats_ateam
 from features import days_break, x_game_average_break
+from aggregate_player_data import read_player_data,aggregate_player_data
 
 d = dirname(dirname(dirname(abspath(__file__))))
 save_data_path = d + '/bench/'
@@ -20,15 +21,19 @@ def main():
 
     # Add form data to the dataset. This includes wins, percentage and points
     # for/against over the last x games (to be defined by user)
-    matches = last_x_games_stats_hteam(matches=matches,history=history_object,num_games=5)
-    matches = last_x_games_stats_ateam(matches=matches,history=history_object,num_games=5)
+    # matches = last_x_games_stats_hteam(matches=matches,history=history_object,num_games=5)
+    # matches = last_x_games_stats_ateam(matches=matches,history=history_object,num_games=5)
 
     # Add days break since last game, and average break over the past 3 games
     matches = days_break(matches)
-    matches = x_game_average_break(matches,num_games=3)
+    # matches = x_game_average_break(matches,num_games=3)
+
+    # Read in player data and the aggregate individual stats to team level
+    players = read_player_data()
+    matches = aggregate_player_data(matches,players)
 
     # Write out the new dataset
-    matches.to_csv(save_data_path + 'matches_with_ladders_form_breaks.csv')
+    matches.to_csv(save_data_path + 'matches_with_player_data.csv')
     print("Ladder/form/breaks file successfully written")
 
 if __name__=="__main__":
