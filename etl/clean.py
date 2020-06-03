7722a0d2a28485f9e7a46b7b903ecdfc54b69f3b
Chris <chris.strods@gmail.com>
2020-06-03 21:56:12 +1000

    Create clean.py

---
+++ etl/clean.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  3 18:16:31 2020
+
+@author: chris
+"""
+
+
+import pandas as pd
+from os.path import dirname, abspath
+
+try:
+    import shared_functions as f
+except ModuleNotFoundError:
+    from etl import shared_functions as f
+
+
+def main():
+    #load files
+    #d = dirname(dirname(abspath(__file__)))
+    d = ".."
+
+    summaries = pd.read_csv(d+"/staging/match_summaries.csv")
+    player_stats = pd.read_csv(d+"/staging/player_stats.csv",low_memory=False)
+    odds = pd.read_csv(d+"/staging/odds_data.csv")
+    fantasy = pd.read_csv(d+"/staging/fantasy_scores.csv")
+    adv_stats = pd.read_csv(d+"/staging/adv_stats.csv")
+    quarters = pd.read_csv(d+"/staging/q_lengths.csv")
+    progression = pd.read_csv(d+"/staging/scoring_progression.csv")
+    
+    #Drop any records which have been scraped twice
+    summaries.drop_duplicates(inplace=True)
+    player_stats.drop_duplicates(inplace=True)
+    odds.drop_duplicates(inplace=True)
+    fantasy.drop_duplicates(inplace=True)
+    adv_stats.drop_duplicates(inplace=True)
+    quarters.drop_duplicates(inplace=True)
+    progression.drop_duplicates(inplace=True)
+    
+    summaries.to_csv(d+"/staging/match_summaries.csv")
+    player_stats.to_csv(d+"/staging/player_stats.csv")
+    odds.to_csv(d+"/staging/odds_data.csv")
+    fantasy.to_csv(d+"/staging/fantasy_scores.csv")
+    adv_stats.to_csv(d+"/staging/adv_stats.csv")
+    quarters.to_csv(d+"/staging/q_lengths.csv")
+    progression.to_csv(d+"/staging/scoring_progression.csv")    
+
+    
+main()
