ebc790d28ce082b88def1b09c75016d273e1dd62
Harry <travershh@gmail.com>
2020-06-04 23:18:18 +1000

    Added tipping engine functionality

---
+++ tipengine/resources/team_name_map.py
@@ -0,0 +1,9 @@
+team_name_map = {
+    'Western Bulldogs': 'Footscray',
+    'Adelaide Crows': 'Adelaide',
+    'GWS Giants': 'Greater Western Sydney',
+    'Gold Coast Suns': 'Gold Coast',
+    'West Coast Eagles': 'West Coast',
+    'Sydney Swans': 'Sydney',
+    'Geelong Cats': 'Geelong'
+}
