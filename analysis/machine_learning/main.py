b2300bbab9da34da55f8c817e30c58e6ff35386f
Harry <travershh@gmail.com>
2020-06-09 09:48:02 +1000

    Added OCR

--- analysis/machine_learning/main.py
+++ analysis/machine_learning/main.py
@@ -21,7 +21,7 @@ import pandas as pd
 def main():
     start_time = time.time()
     # Create the dataframe
-    matches = prepare_dataframe.prepare_dataframe()
+    # matches = prepare_dataframe.prepare_dataframe()
 
     # Use the below line if you need to recalculate the history object
     # The history object contains a hierarchy for traversing data. 
@@ -30,25 +30,25 @@ def main():
     # history = generate_history.generate_history_obj()
 
     # Add ladder information to each round
-    ladder_history = generate_ladders.add_ladders_to_history()
+    # ladder_history = generate_ladders.add_ladders_to_history()
 
     # Add the ladders from ladder_history to the dataframe as an object
-    matches = add_ladders(ladder_history,matches)
+    # matches = add_ladders(ladder_history,matches)
 
     # Get the information out of the ladder objects, and turn them into columns
-    matches = extract_ladder_data(matches, save_to_file=True)
+    # matches = extract_ladder_data(matches, save_to_file=True)
 
     # Get the team form for each team
-    matches = ladder_form.get_ladder_form(ladder_history, matches, 5)
+    # matches = ladder_form.get_ladder_form(ladder_history, matches, 5)
 
     # For testing purposes
-    # matches = pd.read_csv('/Users/t_raver9/Desktop/projects/aflengine/analysis/machine_learning/src/ladder_form/data/matches_with_form.csv')
+    matches = pd.read_csv('/Users/t_raver9/Desktop/projects/aflengine/analysis/machine_learning/src/ladder_form/data/matches_with_form.csv')
     
     # Read player data
     players = player_aggregator.read_data()
 
     # Add player individual statistic totals for season and career
-    players = player_stat_totals.add_player_totals(matches, players)
+    # players = player_stat_totals.add_player_totals(matches, players)
 
     # Aggregate the individual player data to the team level
     matches = player_aggregator.aggregate_player_data(players, matches)
